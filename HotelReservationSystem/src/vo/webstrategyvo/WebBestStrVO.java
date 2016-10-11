7e0d140256175c85684b11d935bb3b83670db56d
cccyf <731767381@qq.com>
2016-10-11 19:44:09 +0800

    strategy

--- HotelReservationSystem/src/vo/webstrategyvo/WebBestStrVO.java
+++ HotelReservationSystem/src/vo/webstrategyvo/WebBestStrVO.java
@@ -1,5 +1,21 @@
 package vo.webstrategyvo;
 
-public class WebBestStrVO {
+import common.Strategy;
 
+/**
+ * 网站最优策略
+ * @author CYF
+ * @version 1.0
+ */
+public class WebBestStrVO {
+	private Strategy bestStr;
+	public WebBestStrVO(Strategy bestStr){
+		this.setBestStr(bestStr);
+	}
+	public Strategy getBestStr() {
+		return bestStr;
+	}
+	public void setBestStr(Strategy bestStr) {
+		this.bestStr = bestStr;
+	}
 }
--- HotelReservationSystem/src/vo/webstrategyvo/WebStrVO.java
+++ HotelReservationSystem/src/vo/webstrategyvo/WebStrVO.java
@@ -1,5 +1,12 @@
 package vo.webstrategyvo;
+import common.Strategy;
 
+/**
+ * 会员等级规则vo
+ * @author CYF
+ * @version 1.0
+ */
 public class WebStrVO {
+	
 
 }
