f135bf15cf37a30b77155066c0e96b2c03432638
cccyf <731767381@qq.com>
2016-10-11 19:30:50 +0800

    又一个vo

--- HotelReservationSystem/src/vo/webstrategyvo/GradeRuleVO.java
+++ HotelReservationSystem/src/vo/webstrategyvo/GradeRuleVO.java
@@ -1,5 +1,24 @@
 package vo.webstrategyvo;
 
+import java.util.Map;
+/**
+ * 等级规则vo
+ * @author CYF
+ * @version 1.0
+ */
 public class GradeRuleVO {
+	//等级规则
+	private Map<Integer,Integer> gradeAndValue;
 
+	public GradeRuleVO(Map<Integer,Integer> gradeAndValue){
+		this.setGradeAndValue(gradeAndValue);
+	}
+	public Map<Integer, Integer> getGradeAndValue() {
+		return gradeAndValue;
+	}
+
+	public void setGradeAndValue(Map<Integer, Integer> gradeAndValue) {
+		this.gradeAndValue = gradeAndValue;
+	}
+	
 }
