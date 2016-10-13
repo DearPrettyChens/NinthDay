dc7d73dd5a4e17da2052b22dd60771a3629a1d28
cccyf <731767381@qq.com>
2016-10-13 16:23:15 +0800

    webStrategy增加类型

--- HotelReservationSystem/src/vo/webstrategyvo/WebBestStrVO.java
+++ HotelReservationSystem/src/vo/webstrategyvo/WebBestStrVO.java
@@ -1,6 +1,7 @@
 package vo.webstrategyvo;
 
 import common.Strategy;
+import common.WebStrategyType;
 
 /**
  * 网站最优策略
@@ -10,15 +11,25 @@ import common.Strategy;
 public class WebBestStrVO {
 	//最优策略
 	private Strategy bestStr;
+	//网站营销策略类型 感觉写到Strategy里面会不会好一点？
+	private WebStrategyType webStrategyType;
+	
 	public WebBestStrVO(){
 	}
-	public WebBestStrVO(Strategy bestStr){
+	public WebBestStrVO(Strategy bestStr,WebStrategyType webStrategyType){
 		this.setBestStr(bestStr);
+		this.setWebStrategyType(webStrategyType);
 	}
 	public Strategy getBestStr() {
 		return bestStr;
 	}
 	public void setBestStr(Strategy bestStr) {
 		this.bestStr = bestStr;
+	}
+	public WebStrategyType getWebStrategyType() {
+		return webStrategyType;
+	}
+	public void setWebStrategyType(WebStrategyType webStrategyType) {
+		this.webStrategyType = webStrategyType;
 	}
 }
