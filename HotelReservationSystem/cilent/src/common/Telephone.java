1f7895ab86521e4a147eb8d6506ff301d77b11d3
Green-Cherry <2423964104@qq.com>
2016-10-29 17:41:30 +0800

    删掉不好的telephone和password

--- HotelReservationSystem/cilent/src/common/Telephone.java
+++ HotelReservationSystem/cilent/src/common/Telephone.java
@@ -1,75 +1,41 @@
-<<<<<<< HEAD
+
 package common;
 
 import common.ResultMessage;
 
 /**
  * 联系方式类
+ * 
  * @author CYF
  * @version 1.0
  */
 public class Telephone {
-	//联系方式
+	// 联系方式
 	private String telephone;
-	//构造方法
+
+	// 构造方法
 	public Telephone(String tel) {
-		this.telephone=tel;
+		this.telephone = tel;
 	}
+
 	/**
 	 * 检查是否有效
+	 * 
 	 * @return ResultMessage
 	 */
-	public ResultMessage checkValid(){
-		if(telephone.length()==11){
+	public ResultMessage checkValid() {
+		if (telephone.length() == 11) {
 			return ResultMessage.SUCCESS;
 		}
 		return ResultMessage.FAIL;
 	}
 
-}
-=======
-package common;
+	public String getTelephone() {
+		return telephone;
+	}
 
-/**电话号码
- * @author cy
- * @version 1.0
- * 
- */
-public class Telephone {
-	
-	    private String telephone;
-	    
-	    public Telephone(){
-	    	
-	    }
-	    public Telephone(String telephone){
-	    	this.telephone=telephone;
-	    	
-	    }
-
-	    
-	    /**
-	     * 检查密码格式是否正确
-	     * @param password
-	     * @return boolean
-	     * @throws 未定
-	     */
-	    public boolean checkFormate(String telephone){
-	    	
-	    	 return true;
-	    	 
-	    }
-
-		public String getPassword() {
-			return telephone;
-		}
-
-		public void setPassword(String password) {
-			this.telephone = telephone;
-		}
-	    
-		
-	
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
 }
->>>>>>> origin/master
